a7594d70010accc682dad93ce4a34bfaf3785e2a
Nico Koprowski <nico.koprowski@daimler.com>
2022-01-11 14:52:19 +0100

    add status to business partner

--- src/main/kotlin/com/catenax/gpdm/entity/BusinessPartner.kt
+++ src/main/kotlin/com/catenax/gpdm/entity/BusinessPartner.kt
@@ -16,5 +16,16 @@ class BusinessPartner(
         joinColumns = [ JoinColumn(name = "partner_id") ],
         inverseJoinColumns = [JoinColumn(name = "identifier_id")]
     )
-    val identifiers: Set<Identifier>
+    val identifiers: Set<Identifier>,
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    val status: BusinessPartnerStatus?
 ) : BaseEntity()
+
+enum class BusinessPartnerStatus(val description: String){
+    ACTIVE("The business partner is active."),
+    DISSOLVED("The business partner is no longer active as the result of an insolvency or in liquidation process."),
+    IN_LIQUIDATION("Liquidation is the process by which a company is brought to an end. The assets and property of the company are redistributed."),
+    INACTIVE("Generic status that indicates that a business partner existed in the past, but is as of today not operational."),
+    INSOLVENCY("Status is assigned if a business partner is in the state of being unable to pay the money owed on time.")
+}
