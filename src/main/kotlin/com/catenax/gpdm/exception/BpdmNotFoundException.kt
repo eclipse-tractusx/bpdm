6a418c1339c222531237e8590fd4562a33c5c56e
Nico Koprowski <nico.koprowski@daimler.com>
2022-01-27 10:47:44 +0100

    add response status to not found exception

--- src/main/kotlin/com/catenax/gpdm/exception/BpdmNotFoundException.kt
+++ src/main/kotlin/com/catenax/gpdm/exception/BpdmNotFoundException.kt
@@ -3,6 +3,7 @@ package com.catenax.gpdm.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class BpdmNotFoundException (
     val objectType: String,
     val identifier: String
