91e92f84c2c79773237da14bef13b87ed4f236fc
Nico Koprowski <nico.koprowski@daimler.com>
2022-02-23 13:47:45 +0100

    add authorization for post requests

--- src/main/kotlin/com/catenax/gpdm/config/SecurityConfig.kt
+++ src/main/kotlin/com/catenax/gpdm/config/SecurityConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -66,6 +67,7 @@ class KeycloakSecurityConfig: KeycloakWebSecurityConfigurerAdapter() {
             .authorizeRequests()
                 .antMatchers("/v3/api-docs/**").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.POST).hasRole("add_company_data")
                 .anyRequest().authenticated()
     }
 }
