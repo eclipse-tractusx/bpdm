746b53fc35782498f812bc7aa568cfcc4769c5ec
Nico Koprowski <nico.koprowski@daimler.com>
2022-03-24 09:38:55 +0100

    docs(OpenAPI): add OpenAPI documentation to CDQ and Elasticsearch controllers

--- src/main/kotlin/com/catenax/gpdm/component/elastic/impl/controller/ElasticSearchController.kt
+++ src/main/kotlin/com/catenax/gpdm/component/elastic/impl/controller/ElasticSearchController.kt
@@ -2,6 +2,10 @@ package com.catenax.gpdm.component.elastic.impl.controller
 
 import com.catenax.gpdm.dto.elastic.BusinessPartnerDoc
 import com.catenax.gpdm.component.elastic.impl.service.ElasticSyncService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,11 +22,25 @@ class ElasticSearchController(
     val elasticSyncService: ElasticSyncService
 ) {
 
+    @Operation(summary = "Index new business partner records on Elasticsearch",
+        description = "Triggers an export of business partner records from BPDM to Elasticsearch. " +
+                "Only exports records which have been updated since the last export. ")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Export of records successfully"),
+        ApiResponse(responseCode = "500", description = "Export failed (no connection to Elasticsearch or database)", content = [Content()])
+    ])
     @PostMapping("/business-partners")
     fun export(): Collection<BusinessPartnerDoc> {
         return elasticSyncService.exportPartnersToElastic()
     }
 
+    @Operation(summary = "Clear business partner index on Elasticsearch",
+        description = "Deletes all business partner records in the Elasticsearch index. " +
+                "Also resets the timestamp from the last export.")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Index successfully cleared"),
+        ApiResponse(responseCode = "500", description = "Clearing failed (no connection to Elasticsearch or database)", content = [Content()])
+    ])
     @DeleteMapping("/business-partners")
     fun clear(){
         return elasticSyncService.clearElastic()
