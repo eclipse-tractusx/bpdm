70fba1c9e2f6b8cef13b189b776e2067e2f26973
Nico Koprowski <nico.koprowski@daimler.com>
2022-07-25 16:02:36 +0200

    feat(Autoconfiguration): add autoconfiguration to common library

---
+++ bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/config/BpdmCommonAutoConfiguration.kt
@@ -0,0 +1,10 @@
+package org.eclipse.tractusx.bpdm.common.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan
+@ConfigurationPropertiesScan
+class BpdmCommonAutoConfiguration
--- bpdm-gate/src/main/kotlin/org/eclipse/tractusx/bpdm/gate/Application.kt
+++ bpdm-gate/src/main/kotlin/org/eclipse/tractusx/bpdm/gate/Application.kt
@@ -4,17 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    scanBasePackages = [
-        "org.eclipse.tractusx.bpdm.gate",
-        "org.eclipse.tractusx.bpdm.common"
-    ]
-)
-@ConfigurationPropertiesScan(
-    basePackages = [
-        "org.eclipse.tractusx.bpdm.gate",
-        "org.eclipse.tractusx.bpdm.common"]
-)
+@SpringBootApplication
+@ConfigurationPropertiesScan
 class Application
 
 fun main(args: Array<String>) {
