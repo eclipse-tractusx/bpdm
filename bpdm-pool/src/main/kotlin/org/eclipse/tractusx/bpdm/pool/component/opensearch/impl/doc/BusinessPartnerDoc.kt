db11c8168ec60661ac538645fbfcc519310ec0b3
Patrick Zeller <patrick.zeller@exxcellent.de>
2022-07-29 12:53:23 +0200

    fix(opensearch): remove bpn from indexed document
    id does not need to be in document source itself

--- bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/component/opensearch/impl/doc/BusinessPartnerDoc.kt
+++ bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/component/opensearch/impl/doc/BusinessPartnerDoc.kt
@@ -1,8 +1,11 @@
 package org.eclipse.tractusx.bpdm.pool.component.opensearch.impl.doc
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 const val BUSINESS_PARTNER_INDEX_NAME = "business-partner"
 
 data class BusinessPartnerDoc(
+    @JsonIgnore // ignore since this is the id and does not need to be in the document source
     val bpn: String,
     val names: Collection<TextDoc>,
     val legalForm: TextDoc?,
