e5e7c824cbbaf6d815fe0f1f993d38e71c578476
Patrick Zeller <patrick.zeller@exxcellent.de>
2022-07-29 10:43:17 +0200

    feat(opensearch): recreate index if mappings changed

--- bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/component/opensearch/impl/service/OpenSearchSyncStarterService.kt
+++ bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/component/opensearch/impl/service/OpenSearchSyncStarterService.kt
@@ -6,21 +6,29 @@ import org.eclipse.tractusx.bpdm.pool.dto.response.SyncResponse
 import org.eclipse.tractusx.bpdm.pool.entity.SyncType
 import org.eclipse.tractusx.bpdm.pool.service.SyncRecordService
 import org.eclipse.tractusx.bpdm.pool.service.toDto
+import org.opensearch.client.RequestOptions
+import org.opensearch.client.RestHighLevelClient
+import org.opensearch.client.indices.CreateIndexRequest
+import org.opensearch.client.indices.GetMappingsRequest
+import org.opensearch.client.indices.GetMappingsResponse
 import org.opensearch.client.opensearch.OpenSearchClient
-import org.opensearch.client.opensearch._types.mapping.TypeMapping
-import org.opensearch.client.opensearch.indices.CreateIndexRequest
 import org.opensearch.client.opensearch.indices.DeleteIndexRequest
+import org.opensearch.cluster.metadata.MappingMetadata
+import org.opensearch.common.xcontent.XContentType
+import org.springframework.context.ApplicationContextException
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.ResourceUtils
+import java.nio.file.Files
 
 @Service
 class OpenSearchSyncStarterService(
     private val syncRecordService: SyncRecordService,
     private val openSearchSyncService: OpenSearchSyncService,
-    private val openSearchClient: OpenSearchClient
+    private val openSearchClient: OpenSearchClient,
+    private val restHighLevelClient: RestHighLevelClient
 ) {
     private val logger = KotlinLogging.logger { }
 
@@ -52,19 +60,55 @@ class OpenSearchSyncStarterService(
     fun clearOpenSearch() {
         logger.info { "Recreating the OpenSearch index" }
 
-        deleteIndex()
-        createIndex()
+        deleteIndexIfExists(BUSINESS_PARTNER_INDEX_NAME)
+        createIndex(BUSINESS_PARTNER_INDEX_NAME)
 
         syncRecordService.reset(syncRecordService.getOrCreateRecord(SyncType.OPENSEARCH))
     }
 
+    /**
+     * Checks whether the existing business partner index is up-to-date with the current index mappings and recreates the index if necessary
+     *
+     * @throws [ApplicationContextException] shutting down the application on exception
+     */
     @EventListener(ContextRefreshedEvent::class)
-    fun createOnInit() {
-        val indexAlreadyExists = openSearchClient.indices().exists { it.index(BUSINESS_PARTNER_INDEX_NAME) }.value()
+    fun updateOnInit() {
 
-        if (!indexAlreadyExists) {
-            createIndex()
+        logger.info { "Checking whether OpenSearch index needs to be recreated..." }
+        try {
+            val indexAlreadyExists = openSearchClient.indices().exists { it.index(BUSINESS_PARTNER_INDEX_NAME) }.value()
+
+            if (!indexAlreadyExists) {
+                logger.info { "Create index as it does not exist yet" }
+                createIndex(BUSINESS_PARTNER_INDEX_NAME)
+            } else {
+                val tempIndexName = "temp-business-partner"
+                deleteIndexIfExists(tempIndexName)
+                createIndex(tempIndexName)
+
+                val existingMappingMetadata = getIndexMappings(BUSINESS_PARTNER_INDEX_NAME).sourceAsMap()
+                val requiredMappingMetadata = getIndexMappings(tempIndexName).sourceAsMap()
+
+                if (requiredMappingMetadata != existingMappingMetadata) {
+                    clearOpenSearch()
+                } else {
+                    logger.info { "Index mappings still up-to-date" }
+                }
+
+                deleteIndexIfExists(tempIndexName)
+            }
+        } catch (e: Throwable) {
+            // make sure Application exits when exception is thrown
+            throw ApplicationContextException("Exception when updating OpenSearch index during initialization", e)
         }
+
+    }
+
+    private fun getIndexMappings(indexName: String): MappingMetadata {
+        val request = GetMappingsRequest()
+        request.indices(indexName)
+        val getMappingResponse: GetMappingsResponse = restHighLevelClient.indices().getMapping(request, RequestOptions.DEFAULT)
+        return getMappingResponse.mappings()[indexName]!!
     }
 
     /**
@@ -88,17 +132,21 @@ class OpenSearchSyncStarterService(
         return response
     }
 
-    private fun deleteIndex() {
-        val deleteIndexRequest: DeleteIndexRequest = DeleteIndexRequest.Builder().index(BUSINESS_PARTNER_INDEX_NAME).build()
-        openSearchClient.indices().delete(deleteIndexRequest)
+    private fun deleteIndexIfExists(indexName: String) {
+        val indexExists = openSearchClient.indices().exists { it.index(indexName) }.value()
+        if (indexExists) {
+            val deleteIndexRequest: DeleteIndexRequest = DeleteIndexRequest.Builder().index(indexName).build()
+            openSearchClient.indices().delete(deleteIndexRequest)
+        }
     }
 
-    private fun createIndex() {
+    private fun createIndex(indexName: String) {
         val indexFile = ResourceUtils.getFile("classpath:opensearch/index-mappings.json")
-        val jsonpMapper = openSearchClient._transport().jsonpMapper()
-        val jsonParser = jsonpMapper.jsonProvider().createParser(indexFile.inputStream())
-        val createIndexRequest =
-            CreateIndexRequest.Builder().index(BUSINESS_PARTNER_INDEX_NAME).mappings(TypeMapping._DESERIALIZER.deserialize(jsonParser, jsonpMapper)).build()
-        openSearchClient.indices().create(createIndexRequest)
+        val indexJson = Files.readString(indexFile.toPath())
+
+        val request = CreateIndexRequest(indexName)
+        request.mapping(indexJson, XContentType.JSON)
+
+        restHighLevelClient.indices().create(request, RequestOptions.DEFAULT)
     }
 }
