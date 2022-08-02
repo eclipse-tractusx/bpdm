12a502c5a6cda398a9b5aa274557921fb1a2789d
Nico Koprowski <nico.koprowski@daimler.com>
2022-08-02 15:46:32 +0200

    fix(CDQ): make fields of CDQ phone number dto nullable

--- bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/dto/cdq/PhoneNumberCdq.kt
+++ bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/dto/cdq/PhoneNumberCdq.kt
@@ -1,8 +1,8 @@
 package org.eclipse.tractusx.bpdm.common.dto.cdq
 
 data class PhoneNumberCdq(
-    val countryPrefix: String,
-    val number: String,
+    val countryPrefix: String?,
+    val number: String?,
     val type: TypeKeyNameUrlCdq?,
     val value: String
 )
