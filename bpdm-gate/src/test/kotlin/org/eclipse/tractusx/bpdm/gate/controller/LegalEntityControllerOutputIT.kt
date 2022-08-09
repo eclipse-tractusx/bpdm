de6cbdec801e560b12ba1c820a229dbcf0a0c4f0
Patrick Zeller <patrick.zeller@exxcellent.de>
2022-08-09 11:02:47 +0200

    test(gate): test invalid address handling for output routes

--- bpdm-gate/src/test/kotlin/org/eclipse/tractusx/bpdm/gate/controller/LegalEntityControllerOutputIT.kt
+++ bpdm-gate/src/test/kotlin/org/eclipse/tractusx/bpdm/gate/controller/LegalEntityControllerOutputIT.kt
@@ -47,12 +47,12 @@ internal class LegalEntityControllerOutputIT @Autowired constructor(
     }
 
     /**
-     * Given augmented business partner exists in cdq
+     * Given legal entity exists in cdq
      * When getting legal entity by external id via output route
      * Then legal entity mapped to the catena data model should be returned
      */
     @Test
-    fun `get legal entity by external id from output route`() {
+    fun `get legal entity by external id`() {
         val expectedLegalEntity = ResponseValues.legalEntityGateOutput1
 
         wireMockServer.stubFor(
@@ -80,12 +80,12 @@ internal class LegalEntityControllerOutputIT @Autowired constructor(
     }
 
     /**
-     * Given augmented business partner does not exist in cdq
+     * Given legal entity does not exist in cdq
      * When getting legal entity by external id via output route
      * Then "not found" response is sent
      */
     @Test
-    fun `get legal entity by external id from output route, not found`() {
+    fun `get legal entity by external id, not found`() {
         wireMockServer.stubFor(
             post(urlPathMatching(getReadAugmentedBusinessPartnerPath()))
                 .willReturn(
@@ -102,11 +102,11 @@ internal class LegalEntityControllerOutputIT @Autowired constructor(
     }
 
     /**
-     * When cdq api responds with an error status code while fetching augmented business partner by external id
+     * When cdq api responds with an error status code while fetching legal entity by external id
      * Then an internal server error response should be sent
      */
     @Test
-    fun `get legal entity by external id from output route, cdq error`() {
+    fun `get legal entity by external id, cdq error`() {
         wireMockServer.stubFor(
             post(urlPathMatching(getReadAugmentedBusinessPartnerPath()))
                 .willReturn(badRequest())
@@ -119,7 +119,35 @@ internal class LegalEntityControllerOutputIT @Autowired constructor(
     }
 
     /**
-     * Given augmented business partners exists in cdq
+     * Given legal entity without legal address in CDQ
+     * When query by its external ID
+     * Then server error is returned
+     */
+    @Test
+    fun `get legal entity without legal address, expect error`() {
+        val invalidPartner = CdqValues.legalEntity1.copy(addresses = emptyList())
+
+        wireMockServer.stubFor(
+            post(urlPathMatching(getReadAugmentedBusinessPartnerPath()))
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                            objectMapper.writeValueAsString(
+                                AugmentedBusinessPartnerResponseCdq(invalidPartner)
+                            )
+                        )
+                )
+        )
+
+        webTestClient.get().uri(CATENA_OUTPUT_LEGAL_ENTITIES_PATH + "/${CdqValues.legalEntity1Response.externalId}")
+            .exchange()
+            .expectStatus()
+            .is5xxServerError
+    }
+
+    /**
+     * Given legal entities exists in cdq
      * When getting legal entities page via output route
      * Then legal entities page mapped to the catena data model should be returned
      */
@@ -185,7 +213,74 @@ internal class LegalEntityControllerOutputIT @Autowired constructor(
     }
 
     /**
-     * When cdq api responds with an error status code while getting augmented business partners
+     * Given legal entity without legal address in CDQ
+     * When getting legal entities page via output route
+     * Then only valid legal entities on page returned
+     */
+    @Test
+    fun `filter legal entities without legal address`() {
+        val legalEntitiesCdq = listOf(
+            CdqValues.legalEntity1Response,
+            CdqValues.legalEntity2Response,
+            CdqValues.legalEntity1Response.copy(addresses = emptyList())
+        )
+
+        val expectedLegalEntities = listOf(
+            ResponseValues.legalEntityGateOutput1,
+            ResponseValues.legalEntityGateOutput2
+        )
+
+        val limit = 3
+        val startAfter = "Aaa111"
+        val nextStartAfter = "Aaa222"
+        val total = 10
+        val invalidEntries = 1
+
+        wireMockServer.stubFor(
+            get(urlPathMatching(CDQ_MOCK_AUGMENTED_BUSINESS_PARTNER_PATH))
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                            objectMapper.writeValueAsString(
+                                BusinessPartnerCollectionCdq(
+                                    limit = limit,
+                                    startAfter = startAfter,
+                                    nextStartAfter = nextStartAfter,
+                                    total = total,
+                                    values = legalEntitiesCdq
+                                )
+                            )
+                        )
+                )
+        )
+
+        val pageResponse = webTestClient.get()
+            .uri { builder ->
+                builder.path(CATENA_OUTPUT_LEGAL_ENTITIES_PATH)
+                    .queryParam(PaginationStartAfterRequest::startAfter.name, startAfter)
+                    .queryParam(PaginationStartAfterRequest::limit.name, limit)
+                    .build()
+            }
+            .exchange()
+            .expectStatus()
+            .isOk
+            .returnResult<PageStartAfterResponse<LegalEntityGateOutput>>()
+            .responseBody
+            .blockFirst()!!
+
+        assertThat(pageResponse).isEqualTo(
+            PageStartAfterResponse(
+                total = total,
+                nextStartAfter = nextStartAfter,
+                content = expectedLegalEntities,
+                invalidEntries = invalidEntries
+            )
+        )
+    }
+
+    /**
+     * When cdq api responds with an error status code while getting legal entities
      * Then an internal server error response should be sent
      */
     @Test
@@ -202,7 +297,7 @@ internal class LegalEntityControllerOutputIT @Autowired constructor(
     }
 
     /**
-     * When cdq api responds with an error status code while getting augmented business partners
+     * When cdq api responds with an error status code while getting legal entities
      * Then an internal server error response should be sent
      */
     @Test
