1cb3f5343a2699cc99fec528ad3d3499e2d6a74c
Nico Koprowski <nico.koprowski@daimler.com>
2022-08-10 15:35:39 +0200

    fix(Db-Migration): revert changes to database migration files

--- bpdm-pool/src/main/resources/db/migration/V0_0_1_2__create_partner_changelog.sql
+++ bpdm-pool/src/main/resources/db/migration/V0_0_1_2__create_partner_changelog.sql
@@ -1,11 +1,11 @@
 CREATE TABLE partner_changelog_entries
 (
-    id             BIGINT       NOT NULL,
-    uuid           UUID         NOT NULL,
+    id             BIGINT                      NOT NULL,
+    uuid           UUID                        NOT NULL,
     created_at     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated_at     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    changelog_type VARCHAR(255) NOT NULL,
-    bpn            VARCHAR(255) NOT NULL,
+    changelog_type VARCHAR(255)                NOT NULL,
+    bpn            VARCHAR(255)                NOT NULL,
     CONSTRAINT pk_partner_changelog_entries PRIMARY KEY (id)
 );
 
