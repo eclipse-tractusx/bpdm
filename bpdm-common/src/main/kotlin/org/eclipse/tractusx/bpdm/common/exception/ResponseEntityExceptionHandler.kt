17c9310bdd82769df30588c542b25750f9aa1199
Nico Koprowski <nico.koprowski@mercedes-benz.com>
2022-11-09 10:55:25 +0100

    feat(Exception-Handler): add basic exception handler which logs uncaught exceptions

---
+++ bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/exception/ResponseEntityExceptionHandler.kt
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2021,2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.eclipse.tractusx.bpdm.gate.exception
+
+import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import kotlin.reflect.full.findAnnotations
+
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+
+    private val kotlinLogger = KotlinLogging.logger { }
+
+    @ExceptionHandler(value = [Exception::class])
+    protected fun logException(
+        ex: Exception, request: WebRequest
+    ): ResponseEntity<Any>? {
+        val annotations = ex::class.findAnnotations(ResponseStatus::class)
+
+        if (annotations.isEmpty() || annotations.any { it.value == HttpStatus.INTERNAL_SERVER_ERROR }) {
+            //Prints with stack trace
+            kotlinLogger.error(ex) { "Caught internal server error" }
+        } else {
+            //Prints without stack trace
+            logger.debug(ex)
+        }
+
+        return handleException(ex, request)
+    }
+
+}
