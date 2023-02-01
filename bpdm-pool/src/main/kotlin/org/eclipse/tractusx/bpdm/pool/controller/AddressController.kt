379f078bce401600db1a1057c976944e4d289030
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2023-02-01 13:13:59 +0100

    feat(pool-controller): Exception Feedback for create/update  Addresses  Site
    feat(pool-controller): Exception Feedback for create/update  Addresses  Site

--- bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/controller/AddressController.kt
+++ bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/controller/AddressController.kt
@@ -35,6 +35,7 @@ import org.eclipse.tractusx.bpdm.pool.dto.request.AddressPartnerUpdateRequest
 import org.eclipse.tractusx.bpdm.pool.dto.request.PaginationRequest
 import org.eclipse.tractusx.bpdm.pool.dto.response.AddressMatchResponse
 import org.eclipse.tractusx.bpdm.pool.dto.response.AddressPartnerCreateResponse
+import org.eclipse.tractusx.bpdm.pool.dto.response.EntitiesWithErrorsResponse
 import org.eclipse.tractusx.bpdm.pool.service.AddressService
 import org.eclipse.tractusx.bpdm.pool.service.BusinessPartnerBuildService
 import org.springdoc.core.annotations.ParameterObject
@@ -120,7 +121,7 @@ class AddressController(
     fun createAddresses(
         @RequestBody
         requests: Collection<AddressPartnerCreateRequest>
-    ): Collection<AddressPartnerCreateResponse> {
+    ): EntitiesWithErrorsResponse<AddressPartnerCreateResponse> {
         return businessPartnerBuildService.createAddresses(requests)
     }
 
@@ -139,7 +140,7 @@ class AddressController(
     fun updateAddresses(
         @RequestBody
         requests: Collection<AddressPartnerUpdateRequest>
-    ): Collection<AddressPartnerResponse> {
+    ): EntitiesWithErrorsResponse<AddressPartnerResponse> {
         return businessPartnerBuildService.updateAddresses(requests)
     }
 }
