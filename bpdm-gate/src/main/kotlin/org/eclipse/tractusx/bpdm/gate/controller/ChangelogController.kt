3ecd495e75b083dd7ab0e337d7546655983bc2da
alexsilva <alexandre.b.silva@cgi.com>
2023-03-16 12:07:18 +0000

    feat(Gate): Creating ChangelogController objects and entity

---
+++ bpdm-gate/src/main/kotlin/org/eclipse/tractusx/bpdm/gate/controller/ChangelogController.kt
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2021,2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.eclipse.tractusx.bpdm.gate.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.eclipse.tractusx.bpdm.gate.dto.response.LsaType
+import org.eclipse.tractusx.bpdm.gate.service.ChangelogService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.time.Instant
+
+
+@RequestMapping("/api/catena/business-partners/changelog")
+class ChangelogController (
+    private val  changelogService: ChangelogService
+    ) {
+
+    @Operation(
+        summary = "Get business partner changelog entries by list external id, from timestamp or LSA type",
+        description = "Get business partner changelog entries by list external id, from timestamp or LSA type"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "The changelog entries for the specified parameters"),
+            ApiResponse(responseCode = "400", description = "On malformed pagination request", content = [Content()]),
+            ApiResponse(responseCode = "404", description = "No business partner found for specified bpn", content = [Content()])
+        ]
+    )
+    @GetMapping
+    fun getChangelogEntries(
+        @RequestBody externalIds: Collection<String>,
+        @Parameter(description = "From Time") @RequestParam fromTime: Instant,
+        @Parameter(description = "LSA Type") @RequestParam lsaType: LsaType
+    ) {
+        return changelogService.getChangeLog()
+    }
+}
