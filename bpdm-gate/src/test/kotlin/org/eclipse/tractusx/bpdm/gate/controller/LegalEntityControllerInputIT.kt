98caad33b4e31c5cd03f141352feded908d9e924
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2023-05-30 17:23:41 +0200

    feat(api): #181  Gate API: Nameparts fix tests

--- bpdm-gate/src/test/kotlin/org/eclipse/tractusx/bpdm/gate/controller/LegalEntityControllerInputIT.kt
+++ bpdm-gate/src/test/kotlin/org/eclipse/tractusx/bpdm/gate/controller/LegalEntityControllerInputIT.kt
@@ -61,7 +61,8 @@ internal class LegalEntityControllerInputIT @Autowired constructor(
     private val webTestClient: WebTestClient,
     private val objectMapper: ObjectMapper,
     val gateClient: GateClient,
-    private val legalEntityRepository: LegalEntityRepository
+    private val legalEntityRepository: LegalEntityRepository,
+    private val testHelpers: DbTestHelpers
 ) {
     companion object {
         @RegisterExtension
