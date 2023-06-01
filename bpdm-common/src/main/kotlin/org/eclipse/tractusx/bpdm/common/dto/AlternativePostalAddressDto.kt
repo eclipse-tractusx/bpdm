0b40c0d5e944380e7b62422f717318f2400db603
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2023-06-01 11:16:47 +0200

    feat(api): #202 API: Delivery Service Qualifier in Alternative Postal Address

--- bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/dto/AlternativePostalAddressDto.kt
+++ bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/dto/AlternativePostalAddressDto.kt
@@ -40,4 +40,7 @@ data class AlternativePostalAddressDto(
 
     @get:Schema(description = "The type of this specified delivery")
     val deliveryServiceType: DeliveryServiceType = DeliveryServiceType.PO_BOX,
+
+    @get:Schema(description = "Delivery Service Qualifier")
+    val deliveryServiceQualifier: String = "",
 )
