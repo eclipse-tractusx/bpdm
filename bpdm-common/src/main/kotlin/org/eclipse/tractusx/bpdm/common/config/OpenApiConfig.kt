336624c7a17bd438c5560f677e5d92050d3bee12
Martin Kaeser <martin.kaeser@exxcellent.de>
2023-08-22 15:41:18 +0200

    feat(OpenAPI): Add schema overriding hook into OpenApiConfig

--- bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/config/OpenApiConfig.kt
+++ bpdm-common/src/main/kotlin/org/eclipse/tractusx/bpdm/common/config/OpenApiConfig.kt
@@ -19,32 +19,93 @@
 
 package org.eclipse.tractusx.bpdm.common.config
 
+import com.neovisionaries.i18n.CountryCode
+import com.nimbusds.jose.shaded.gson.reflect.TypeToken
+import io.swagger.v3.core.converter.AnnotatedType
+import io.swagger.v3.core.converter.ModelConverters
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.*
 import mu.KotlinLogging
+import org.eclipse.tractusx.bpdm.common.dto.openapidescription.GenericDescription
+import org.eclipse.tractusx.bpdm.common.dto.response.type.TypeKeyNameVerboseDto
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import java.lang.reflect.Type
 
 
-@Configuration
-class OpenApiConfig(
-    val securityProperties: SecurityConfigProperties,
-    val infoProperties: AppInfoProperties
-) {
+abstract class OpenApiConfig {
     private val logger = KotlinLogging.logger { }
 
-    @Bean
-    fun customOpenAPI(): OpenAPI? {
-        return OpenAPI().info(Info().title(infoProperties.name).description(infoProperties.description).version(infoProperties.version))
-            .also { if (securityProperties.enabled) it.withSecurity() }
-    }
+    abstract val securityProperties: SecurityConfigProperties
+    abstract val infoProperties: AppInfoProperties
 
     @Bean
-    fun openApiDefinition(): GroupedOpenApi {
+    open fun customOpenAPI(): OpenAPI? {
+        val info = Info()
+            .title(infoProperties.name)
+            .description(infoProperties.description)
+            .version(infoProperties.version)
+        val components = buildComponents(getSchemaOverrides())
+        return OpenAPI()
+            .info(info)
+            .components(components)
+            .also {
+                if (securityProperties.enabled)
+                    it.withSecurity()
+            }
+    }
+
+    /**
+     * This hooks allows overriding OpenAPI schema definitions.
+     * This can be used for two scenarios:
+     * - Override the schema description of a generic class with specific type parameter, as the annotated Schema description might not be specific enough.
+     * - Clone a schema, overriding its name and description, to use it for some specific field.
+     */
+    open fun getSchemaOverrides(): Collection<SchemaOverrideInfo> {
+        return setOf(
+            // Generic types
+            schemaOverride<TypeKeyNameVerboseDto<CountryCode>>(GenericDescription.typeKeyNameCountryCode)
+        )
+    }
+
+    private fun buildComponents(schemaOverrideInfos: Collection<SchemaOverrideInfo>): Components {
+        val components = Components()
+        schemaOverrideInfos.forEach {
+            val schemaCopy = copySchema(it.schemaType)
+            schemaCopy.description(it.description)
+            it.alternativeSchemaName?.let { altSchemaName ->
+                schemaCopy.name(altSchemaName)
+            }
+            components.addSchemas(schemaCopy.name, schemaCopy)
+        }
+        return components
+    }
+
+    private fun copySchema(schemaType: Type) =
+        ModelConverters.getInstance()
+            .resolveAsResolvedSchema(
+                AnnotatedType(schemaType).resolveAsRef(false)
+            )
+            .schema
+
+    inline fun <reified T> schemaOverride(description: String, alternativeSchemaName: String? = null) =
+        SchemaOverrideInfo(
+            object : TypeToken<T>() {}.type,
+            description,
+            alternativeSchemaName
+        )
+
+    data class SchemaOverrideInfo(
+        val schemaType: Type,
+        val description: String,
+        val alternativeSchemaName: String? = null
+    )
+
+    @Bean
+    open fun openApiDefinition(): GroupedOpenApi {
         return GroupedOpenApi.builder()
             .group("docs")
             .pathsToMatch("/**")
@@ -55,7 +116,9 @@ class OpenApiConfig(
 
     fun sortSchemaCustomiser(): OpenApiCustomizer {
         return OpenApiCustomizer { openApi: OpenAPI ->
-            openApi.components(with(openApi.components) { schemas(schemas?.values?.sortedBy { it.name }?.associateBy { it.name }) })
+            with(openApi.components) {
+                schemas(schemas.toSortedMap())
+            }
         }
     }
 
