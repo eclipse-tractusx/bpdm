a3034e1312e2b423bd07bbe704e9014126d4ae45
Nico Koprowski <nico.koprowski@abat.de>
2026-04-10 12:00:34 +0800

    test: increase pool size

--- bpdm-common-test/src/main/kotlin/org/eclipse/tractusx/bpdm/test/containers/PostgreSQLContextInitializer.kt
+++ bpdm-common-test/src/main/kotlin/org/eclipse/tractusx/bpdm/test/containers/PostgreSQLContextInitializer.kt
@@ -37,6 +37,7 @@ class PostgreSQLContextInitializer : ApplicationContextInitializer<ConfigurableA
                 .parse("docker.io/postgres:18.3@sha256:a9abf4275f9e99bff8e6aed712b3b7dfec9cac1341bba01c1ffdfce9ff9fc34a")
                 .asCompatibleSubstituteFor("postgres")
             )
+            .withCommand("max_connections=200")
             .withAccessToHost(true)
             .withNetwork(Network.SHARED)
     }
