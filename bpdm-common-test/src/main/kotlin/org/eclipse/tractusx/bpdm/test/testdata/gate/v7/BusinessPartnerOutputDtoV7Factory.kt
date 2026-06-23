548736684a270b169a6cc0c162df37da657b9092
MohanKumar JN <capgemini.mohankumar@mercedes-benz.com>
2026-06-23 20:41:52 +0530

    changes on v7 factory and result factory

--- bpdm-common-test/src/main/kotlin/org/eclipse/tractusx/bpdm/test/testdata/gate/v7/BusinessPartnerOutputDtoV7Factory.kt
+++ bpdm-common-test/src/main/kotlin/org/eclipse/tractusx/bpdm/test/testdata/gate/v7/BusinessPartnerOutputDtoV7Factory.kt
@@ -133,7 +133,8 @@ class BusinessPartnerOutputDtoV7Factory {
             shortName = legalEntity.legalShortName,
             legalForm = legalEntity.legalForm,
             confidenceCriteria = buildConfidence(legalEntity.confidenceCriteria),
-            states = legalEntity.states.map { BusinessPartnerStateDto(it.validFrom, it.validTo, it.type) }
+            states = legalEntity.states.map { BusinessPartnerStateDto(it.validFrom, it.validTo, it.type) },
+            updatedAt = legalEntity.updatedAt
         )
     }
 
@@ -145,7 +146,8 @@ class BusinessPartnerOutputDtoV7Factory {
             physicalPostalAddress = buildPhysicalAddress(logisticAddress.physicalPostalAddress),
             alternativePostalAddress = logisticAddress.alternativePostalAddress?.let { buildAlternativeAddress(it) },
             confidenceCriteria = buildConfidence(logisticAddress.confidenceCriteria),
-            states = logisticAddress.states.map { BusinessPartnerStateDto(it.validFrom, it.validTo, it.type) }
+            states = logisticAddress.states.map { BusinessPartnerStateDto(it.validFrom, it.validTo, it.type) },
+            updatedAt = logisticAddress.updatedAt
         )
     }
 
@@ -201,7 +203,8 @@ class BusinessPartnerOutputDtoV7Factory {
             siteBpn = site.bpns,
             name = site.name,
             confidenceCriteria = buildConfidence(site.confidenceCriteria),
-            states = site.states.map { BusinessPartnerStateDto(it.validFrom, it.validTo, it.type) }
+            states = site.states.map { BusinessPartnerStateDto(it.validFrom, it.validTo, it.type) },
+            updatedAt = site.updatedAt
         )
     }
 
