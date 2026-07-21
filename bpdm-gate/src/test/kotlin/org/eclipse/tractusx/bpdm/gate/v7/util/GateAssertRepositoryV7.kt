868a38af664cb88c38eeb98acb699b706e6c932c
MohanKumar JN <capgemini.mohankumar@mercedes-benz.com>
2026-07-21 16:23:25 +0530

    Fixed the test failure by adding nested updatedAt

--- bpdm-gate/src/test/kotlin/org/eclipse/tractusx/bpdm/gate/v7/util/GateAssertRepositoryV7.kt
+++ bpdm-gate/src/test/kotlin/org/eclipse/tractusx/bpdm/gate/v7/util/GateAssertRepositoryV7.kt
@@ -55,7 +55,9 @@ class GateAssertRepositoryV7(
             .usingRecursiveComparison()
             .ignoringFields(
                 BusinessPartnerOutputDto::createdAt.name,
-                BusinessPartnerOutputDto::updatedAt.name
+                BusinessPartnerOutputDto::updatedAt.name,
+                "legalEntity.updatedAt",
+                "address.updatedAt"
             )
             .withComparatorForType(instantSecondsComparator, Instant::class.java)
             .withComparatorForType(localDatetimeSecondsComparator, LocalDateTime::class.java)
